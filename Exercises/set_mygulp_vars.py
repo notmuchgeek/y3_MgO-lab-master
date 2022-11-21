e18eb5f7724e686334e895f298d8d63e2e587e3d
notmuchgeek <xiaocen.dong@hotmail.com>
2022-11-21 11:59:40 +0000

    delete and upload all

--- Exercises/set_mygulp_vars.py
+++
@@ -1,3 +0,0 @@
-import os
-os.environ['GULP_LIB'] =r'C:\Users\chemistry\MgO-lab-master\set_up'
-os.environ['ASE_GULP_COMMAND'] =r'C:\Users\chemistry\MgO-lab-master\gulp_dir\gulp-6.0\Exe\gulp.exe < PREFIX.gin > PREFIX.got'
--- gulp_dir/gulp-6.0/Src/csignal.c
+++
@@ -1,8 +0,0 @@
-#ifdef CTRLC
-#include <signal.h>
-typedef void (*sighandler_t)(int);
-void csignal_( int* signum, sighandler_t handler)
-{
- signal(*signum, handler);
-}
-#endif
