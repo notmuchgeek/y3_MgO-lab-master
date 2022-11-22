97d8e592a2158af7178894dd144022251578bb87
notmuchgeek <xiaocen.dong@hotmail.com>
2022-11-22 09:11:46 +0000

    Monday Work

--- MgO-lab-master/Exercises/set_mygulp_vars.py
+++ MgO-lab-master/Exercises/set_mygulp_vars.py
@@ -1,3 +1,3 @@
 import os
-os.environ['GULP_LIB'] =r'C:\Users\chemistry\MgO-lab-master\set_up'
-os.environ['ASE_GULP_COMMAND'] =r'C:\Users\chemistry\MgO-lab-master\gulp_dir\gulp-6.0\Exe\gulp.exe < PREFIX.gin > PREFIX.got'
+os.environ['GULP_LIB'] =r'C:\Users\AlbertCielstian\Documents\GitHub\y3_MgO-lab-master\MgO-lab-master\set_up'
+os.environ['ASE_GULP_COMMAND'] =r'C:\Users\AlbertCielstian\Documents\GitHub\y3_MgO-lab-master\MgO-lab-master\gulp_dir\gulp-6.0\Exe\gulp.exe < PREFIX.gin > PREFIX.got'
